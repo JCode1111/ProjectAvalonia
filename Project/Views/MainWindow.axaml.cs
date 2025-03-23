3b7c6cbec779d47882a12e90f4233455c971eca3
JCode1111 <kruzlakjakub6@gmail.com>
2025-03-23 22:51:00 +0100

    add menu

--- Project/Views/MainWindow.axaml.cs
+++ Project/Views/MainWindow.axaml.cs
@@ -1,11 +1,37 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
 
-namespace Project.Views;
-
-public partial class MainWindow : Window
+namespace Project.Views
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();  // Inicjalizuje kontrolki z XAML
+            this.WindowState = WindowState.Maximized;  // Ustawia okno na pełny ekran
+        }
+
+        // Obsługuje kliknięcie w "Witaj użytkowniku"
+        private void WitajUzytkownikuMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ContentArea.Content = new TextBlock
+            {
+                Text = "Witaj użytkowniku!",
+                HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+                VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center
+            };
+        }
+
+        // Obsługuje kliknięcie w "Saldo"
+        private void SaldoMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ContentArea.Content = new TextBlock
+            {
+                Text = "Twoje saldo wynosi: 5000 PLN",
+                HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+                VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center
+            };
+        }
     }
 }
